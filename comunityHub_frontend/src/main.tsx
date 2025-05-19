d85027f3ef1c062e4f0a5ff870bf512d9a318977
jhordan <jhordan.suzuki@lmcorp.local>
2025-05-18 22:53:38 -0300

    feat: installing dependences and config setup front-end up

---
+++ comunityHub_frontend/src/main.tsx
@@ -0,0 +1,56 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css';
+import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from '@clerk/clerk-react'
+import { BrowserRouter, Route, Routes, useNavigate, type To } from 'react-router-dom'
+import HomePage from './pages/HomePage.tsx'
+import RootLayout from './layouts/RootLayout.tsx'
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <SignedIn>{children}</SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
+  )
+}
+
+const RouterComponent = () => {
+   const navigate = useNavigate()
+
+   return (
+     <ClerkProvider
+      publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route path="" element={<RootLayout />} /> 
+          <Route
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route />
+      </Routes>
+    </ClerkProvider>
+   )
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+     <MantineProvider>
+        <BrowserRouter>
+          <RouterComponent />
+        </BrowserRouter>
+     </MantineProvider>
+  </StrictMode>,
+)
+
+export default RouterComponent;
